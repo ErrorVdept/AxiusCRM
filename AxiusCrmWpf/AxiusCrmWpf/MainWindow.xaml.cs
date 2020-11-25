76b6b2e1a3c0b784f1342aea7515eeb424877cee
ErrorVdept <74969707+ErrorVdept@users.noreply.github.com>
2020-11-25 17:10:32 +0800

    1'st ver
    Дизайн меню и часть окна клиенты

---
+++ AxiusCrmWpf/AxiusCrmWpf/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AxiusCrmWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ClientPage clientPage = new ClientPage();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnClient_Click(object sender, RoutedEventArgs e)
+        {
+            pageFrame.Navigate(clientPage);
+        }
+
+    }
+}
